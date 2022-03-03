a51de85c78a9b21706cc7ca336465fd99555c00b
GteWill <823063526@qq.com>
2022-03-03 22:44:47 +0800

    启动器

---
+++ src/main/java/HaircutApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author will
+ */
+@SpringCloudApplication
+public class HaircutApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HaircutApplication.class,args);
+    }
+}
